670ceeaa9c2a87a91beee6cfce4fc6b14393fddb
peng <dianxiangan@aliyun.com>
2016-08-11 01:22:55 +0800

    shell

--- build.sh
+++ build.sh
@@ -5,5 +5,12 @@ project="user-biz"
 dir="$project/target/"
 jar=`ls ${dir} | grep '.*\.jar$'`
 jarPath=${dir} $jar
-echo $jarPath
-java -jar $jarPath &
+echo $1
+if [ $1 = "stop" ] ##inside [] must have space!
+then
+	pid=`ps aux | grep java | grep -i ${jar} | awk '{print \$2}'`
+	kill -9 ${pid}
+	echo "============= stop service ${jar} =========="
+else
+	java -jar $jarPath &
+fi
