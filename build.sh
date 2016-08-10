dc3204cb7171f09e4267ac9bd1f72ab8182f7ecd
peng <dianxiangan@aliyun.com>
2016-08-11 01:45:04 +0800

    shell

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@
 project="user-biz"
 dir="$project/target/"
 jar=`ls ${dir} | grep '.*\.jar$'`
-jarPath=${dir} $jar
-echo $1
-if [ $1 = "stop" ] ##inside [] must have space!
-then
-	pid=`ps aux | grep java | grep -i ${jar} | awk '{print \$2}'`
-	kill -9 ${pid}
-	echo "============= stop service ${jar} =========="
-else
-	java -jar $jarPath &
+if [ ! ${jar}];then
+	echo " no runnable jar was found in ${dir}"
+	exit -1
 fi
+echo			"=============	 process ${jar} =========="
+jarPath="${dir}$jar"
+pid=`ps aux | grep java | grep -i ${jar} | awk '{print \$2}'`
+if [ pid ] ;then
+ 	kill -9 ${pid}
+	echo 		"============= 	stop service ${jar} =========="
+fi
+java -jar $jarPath &
