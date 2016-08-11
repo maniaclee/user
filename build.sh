83e02270a8d27ecc7519de6ba7d061b862ca0b12
peng <dianxiangan@aliyun.com>
2016-08-11 18:39:41 +0800

    shell

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 ## mvn clean install -Dmaven.test.skip=true
+ function print()
+ {
+   echo			"=============	${1} =========="
+ }
 project="user-biz"
 dir="$project/target/"
 jar=`ls ${dir} | grep '.*\.jar$'`
 if [ ! ${jar} ];then
-	echo " no runnable jar was found in ${dir}"
+	print " no runnable jar was found in ${dir}"
 	exit -1
 fi
-echo			"=============	 process ${jar} =========="
+echo			"=============	process ${jar} =========="
 jarPath="${dir}$jar"
 pid=`ps aux | grep java | grep -i ${jar} | awk '{print \$2}'`
-if [ pid ] ;then
+if [ ${pid} ] ;then
  	kill -9 ${pid}
-	echo 		"============= 	stop service ${jar} =========="
+	print 		"============= 	stop service ${jar}  pid : ${pid}=========="
 fi
-java -jar $jarPath
+java -jar ${jarPath} &
